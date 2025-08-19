ba4202c6ba8a6c70a5fe05337dab7da8e9e87e11
Eclipse <Eclips77@users.noreply.github.com>
2025-08-19 17:49:52 +0300

    Fix phone_number type in SoliderCol constructor from int to str

--- services/solider_entity.py
+++ services/solider_entity.py
@@ -3,7 +3,7 @@ import uuid
 class SoliderCol:
     """_summary_
     """
-    def __init__(self,first_name:str,last_name:str,phone_number:int,rank:str) -> None:
+    def __init__(self,first_name:str,last_name:str,phone_number:str,rank:str) -> None:
         self.first_name = first_name
         self.last_name = last_name
         self.phone_number = phone_number
