147dbe4a4207ec8f873cb8cce560807f4b56b57b
Eclipse <Eclips77@users.noreply.github.com>
2025-08-20 12:34:27 +0300

    Refactor Dockerfile to streamline application setup; update api.py to comment out main execution block

--- services/api.py
+++ services/api.py
@@ -131,6 +131,6 @@ async def shutdown_event():
     """
     dal.close_connection()
 
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+# if __name__ == "__main__":
+#     import uvicorn
+#     uvicorn.run(app, host="0.0.0.0", port=8000)
